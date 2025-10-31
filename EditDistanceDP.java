9270cb6b207f1e72975092c6bb6ba047fecc8b63
kashish yadav <51848533+kashishyadav@users.noreply.github.com>
2025-10-31 03:36:14 -0500

    Create EditDistanceDP.java

---
+++ EditDistanceDP.java
@@ -0,0 +1,21 @@
+class EditDistanceDP {
+    public static int minDistance(String a, String b) {
+        int m = a.length(), n = b.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++)
+            for (int j = 0; j <= n; j++) {
+                if (i == 0) dp[i][j] = j;
+                else if (j == 0) dp[i][j] = i;
+                else if (a.charAt(i - 1) == b.charAt(j - 1))
+                    dp[i][j] = dp[i - 1][j - 1];
+                else
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j - 1],
+                                  Math.min(dp[i - 1][j], dp[i][j - 1]));
+            }
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(minDistance("horse", "ros")); // 3
+    }
+}
